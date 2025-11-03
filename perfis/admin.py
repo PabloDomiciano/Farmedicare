d2ce5427c38f027af51b6fb67cdf3314f98a4e13
PabloDomiciano <pablo.domiciano2019@gmail.com>
2025-11-03 15:38:25 -0300

    chore: atualizar admin para suportar sistema multi-tenant
    - Registrar PerfilUsuario no admin
    - Configurar inline para fazendas no admin
    - Melhorar visualização de dados relacionados

--- perfis/admin.py
+++ perfis/admin.py
@@ -1,6 +1,46 @@
 from django.contrib import admin
-from .models import Fazenda, Parceiros
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Fazenda, Parceiros, PerfilUsuario
+
 # Register your models here.
 
-admin.site.register(Fazenda)
-admin.site.register(Parceiros)
+
+class PerfilUsuarioInline(admin.StackedInline):
+    model = PerfilUsuario
+    can_delete = False
+    verbose_name_plural = 'Perfil'
+    filter_horizontal = ('fazendas',)
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (PerfilUsuarioInline,)
+
+
+# Re-registrar UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Fazenda)
+class FazendaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cidade', 'estado', 'dono', 'ativa', 'data_criacao')
+    list_filter = ('ativa', 'estado', 'data_criacao')
+    search_fields = ('nome', 'cidade', 'dono__username', 'dono__first_name', 'dono__last_name')
+    date_hierarchy = 'data_criacao'
+    readonly_fields = ('data_criacao',)
+
+
+@admin.register(Parceiros)
+class ParceirosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'fazenda', 'telefone', 'email')
+    list_filter = ('fazenda',)
+    search_fields = ('nome', 'email', 'telefone')
+
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tipo', 'telefone', 'cpf')
+    list_filter = ('tipo',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'cpf', 'telefone')
+    filter_horizontal = ('fazendas',)
